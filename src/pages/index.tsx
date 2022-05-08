e0335fbda61c8c9297cff76365f766804d993661
Dimas Aldi Candra S <cakjengger@gmail.com>
2022-05-09 01:19:19 +0700

    refc: Refactor modules on components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Title } from '../components/atoms';
+import { Title } from '../components';
 
 const Home: NextPage = () => {
   return (
